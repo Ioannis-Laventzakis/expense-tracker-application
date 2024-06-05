3eda74dae4ba49acd52a55f3d66b413ead00620e
Ioannis Laventzakis <glaventzakis@gmail.com>
2024-06-05 20:16:33 +0200

    updated version

--- src/main/java/com/tracker/expense_tracker_application/repository/AppUserRepository.java
+++ src/main/java/com/tracker/expense_tracker_application/repository/AppUserRepository.java
@@ -4,21 +4,7 @@ import com.tracker.expense_tracker_application.model.AppUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-/**
- * Repository interface for handling operations related to the AppUser entity.
- * This interface extends JpaRepository which provides JPA related methods
- * such as save(), findOne(), findAll(), count(), delete() etc.
- * By extending JpaRepository, we get a bunch of generic CRUD methods into
- * our type that allows saving Users, deleting them and so on.
- */
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    /**
-     * Finds a user by their username.
-     *
-     * @param username the username of the user to find
-     * @return the user with the given username
-     */
-
     AppUser findByUsername(String username);
 }
