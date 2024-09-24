fa68b2896955ddb1c8cd25fb1c21af9bab98c02e
Ioannis <Glaventzakis@gmail.com>
2024-09-24 19:02:39 +0200

    Initial commit

---
+++ src/test/java/controller/UserControllerTest.java
@@ -0,0 +1,25 @@
+package controller;
+
+
+import com.tracker.expense_tracker_application.controller.UserController;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = UserController.class)
+@AutoConfigureMockMvc(addFilters = false)
+@ExtendWith(MockitoExtension.class)
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserController userController;
+
+
+}
